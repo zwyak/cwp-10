a568399079b3ee037762402c9c3a1c45d6d5d8e6
zwyak <vlad.senyuk97@gmail.com>
2020-02-17 10:24:47 +0300

    Added creating film

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const films = require('./top250.json');
-const utils = require('./utils.js')
+let films = require('./top250.json');
+const utils = require('./utils.js');
 const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
@@ -32,11 +32,46 @@ const createFilm = (title, rating, year, budget, gross, poster, position) =>{
   if (position){
     film.position = position;
     const res = films.filter(film => film.position == position);
-    if (res) shiftFilm(position);
+    if (res){
+      console.log('Film positions were shifted');
+      shiftFilm(position);
+    }
   }
 
   films.push(film);
-  utils.writeFile(films);
+  utils.writeJson('./top250.json', JSON.stringify(films));
+  films = require('./top250.json');
+
+  return film;
+}
+
+const updateFilm = (id, title, rating, year, budget, gross, poster, position) =>{
+  const film = films.filter(film => film.id == id);
+
+  if (!film){
+    return '404 - Not Found'
+  }
+
+  if (title) film.title = title;
+  if (rating) film.rating = rating;
+  if (year) film.year = year;
+  if (budget) film.budget = budget;
+  if (gross) film.gross = gross;
+  if (poster) film.poster = poster;
+  if (position){
+    film.position = position;
+    const res = films.filter(film => film.position == position);
+    if (res){
+      console.log('Film positions were shifted');
+      shiftFilm(position);
+    }
+  }
+
+  films.filter( (f) =>{
+    if (f.id == id) f = film;
+  });
+  utils.writeJson('./top250.json', JSON.stringify(films));
+  films = require('./top250.json');
 
   return film;
 }
@@ -52,10 +87,14 @@ app.get('/api/films/read', (req, res) => {
 });
 
 app.post('/api/films/create', (req, res) => {
-  console.log(req.body);
-  //res.send(createFilm(req.body.title, req.body.rating, req.body.year, req.body.budget, req.body.gross, req.body.poster, req.body.position));
+  res.send(createFilm(req.body.title, req.body.rating, req.body.year, req.body.budget, req.body.gross, req.body.poster, req.body.position));
 });
 
+app.post('/api/films/update', (req, res) => {
+  res.send(updateFilm(req.body.id, req.body.title, req.body.rating, req.body.year, req.body.budget, req.body.gross, req.body.poster, req.body.position));
+});
+
+
 app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+  console.log('Server app listening on port 3000!');
 })
